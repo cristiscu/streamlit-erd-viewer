d6094e890fee6e2161d95c961ab34ed69a7eba6c
cristiscu <cristiscu@gmail.com>
2023-09-25 09:58:20 -0700

    added upper cases

--- app.py
+++ app.py
@@ -38,12 +38,14 @@ class Table:
 
 
     @classmethod
-    def getClassName(cls, name):
-        return name.lower() if re.match("^[A-Z_0-9]*$", name) != None else f'"{name}"'
+    def getClassName(cls, name, useUpperCase, withQuotes=True):
+        if re.match("^[A-Z_0-9]*$", name) == None:
+            return f'"{name}"' if withQuotes else name
+        return name.upper() if useUpperCase else name.lower()
 
 
-    def getName(self):
-        return Table.getClassName(self.name)
+    def getName(self, useUpperCase, withQuotes=True):
+        return Table.getClassName(self.name, useUpperCase, withQuotes)
 
 
     def getColumn(self, name):
@@ -53,80 +55,100 @@ class Table:
         return None
 
 
-    def getUniques(self, name):
+    def getUniques(self, name, useUpperCase):
         constraint = self.uniques[name]
-        uniques = [column.getName() for column in constraint]
+        uniques = [column.getName(useUpperCase) for column in constraint]
         ulist = ", ".join(uniques)
-        return (f',\n  constraint {Table.getClassName(name)}\n'
+
+        if useUpperCase:
+            return (f',\n  CONSTRAINT {Table.getClassName(name, useUpperCase)}\n'
+                + f"    UNIQUE ({ulist})")
+        return (f',\n  constraint {Table.getClassName(name, useUpperCase)}\n'
             + f"    unique ({ulist})")
 
 
-    def getPKs(self):
-        pks = [column.getName() for column in self.pks]
+    def getPKs(self, useUpperCase):
+        pks = [column.getName(useUpperCase) for column in self.pks]
         pklist = ", ".join(pks)
         pkconstraint = self.pks[0].pkconstraint
-        return (f',\n  constraint {Table.getClassName(pkconstraint)}\n'
+
+        if useUpperCase:
+            return (f',\n  CONSTRAINT {Table.getClassName(pkconstraint, useUpperCase)}\n'
+                + f"    PRIMARY KEY ({pklist})")
+        return (f',\n  constraint {Table.getClassName(pkconstraint, useUpperCase)}\n'
             + f"    primary key ({pklist})")
 
 
-    def getFKs(self, name):
+    def getFKs(self, name, useUpperCase):
         constraint = self.fks[name]
         pktable = constraint[0].fkof.table
 
-        fks = [column.getName() for column in constraint]
+        fks = [column.getName(useUpperCase) for column in constraint]
         fklist = ", ".join(fks)
-        pks = [column.fkof.getName() for column in constraint]
+        pks = [column.fkof.getName(useUpperCase) for column in constraint]
         pklist = ", ".join(pks)
-        return (f"alter table {self.getName()}\n"
-            + f"  add constraint {Table.getClassName(name)}\n"
+
+        if useUpperCase:
+            return (f"ALTER TABLE {self.getName(useUpperCase)}\n"
+                + f"  ADD CONSTRAINT {Table.getClassName(name, useUpperCase)}\n"
+                + f"  ADD FOREIGN KEY ({fklist})\n"
+                + f"  REFERENCES {pktable.getName(useUpperCase)} ({pklist});\n\n")
+        return (f"alter table {self.getName(useUpperCase)}\n"
+            + f"  add constraint {Table.getClassName(name, useUpperCase)}\n"
             + f"  add foreign key ({fklist})\n"
-            + f"  references {pktable.getName()} ({pklist});\n\n")
+            + f"  references {pktable.getName(useUpperCase)} ({pklist});\n\n")
 
 
     # outputs a CREATE TABLE statement for the current table
-    def getCreateTable(self):
-        s = f"create or replace table {self.getName()} ("
+    def getCreateTable(self, useUpperCase):
+        if useUpperCase:
+            s = f"CREATE OR REPLACE TABLE {self.getName(useUpperCase)} ("
+        else:
+            s = f"create or replace table {self.getName(useUpperCase)} ("
         
         first = True
         for column in self.columns:
             if first: first = False
             else: s += ","
-            s += column.getCreateColumn()
+            s += column.getCreateColumn(useUpperCase)
 
         if len(self.uniques) > 0:
             for constraint in self.uniques:
-                s += self.getUniques(constraint)
+                s += self.getUniques(constraint, useUpperCase)
         if len(self.pks) >= 1:
-            s += self.getPKs()
+            s += self.getPKs(useUpperCase)
         
         s += "\n)"
         if self.comment != '':
             comment = self.comment.replace("'", "''")
-            s += f" comment = '{comment}'"
+            s += f" comment = '{comment}'" if not useUpperCase else f" COMMENT = '{comment}'"
         return s + ";\n\n"
 
 
-    def getDotShape(self, theme, showColumns, showTypes):
+    def getDotShape(self, theme, showColumns, showTypes, useUpperCase):
         fillcolor = theme.fillcolorC if showColumns else theme.fillcolor
         colspan = "2" if showTypes else "1"
+        tableName = self.getName(useUpperCase, False)
         s = (f'  {self.label} [\n'
             + f'    fillcolor="{fillcolor}" color="{theme.color}" penwidth="1"\n'
             + f'    label=<<table style="{theme.style}" border="0" cellborder="0" cellspacing="0" cellpadding="1">\n'
             + f'      <tr><td bgcolor="{theme.bgcolor}" align="center"'
-            + f' colspan="{colspan}"><font color="{theme.tcolor}"><b>{self.name}</b></font></td></tr>\n')
+            + f' colspan="{colspan}"><font color="{theme.tcolor}"><b>{tableName}</b></font></td></tr>\n')
 
         if showColumns:
             for column in self.columns:
-                name = column.name
+                name = column.getName(useUpperCase, False)
                 if column.ispk: name = f"<u>{name}</u>"
                 if column.fkof != None: name = f"<i>{name}</i>"
                 if column.nullable: name = f"{name}*"
                 if column.identity: name = f"{name} I"
                 if column.isunique: name = f"{name} U"
+                datatype = column.datatype
+                if useUpperCase: datatype = datatype.upper()
 
                 if showTypes:
                     s += (f'      <tr><td align="left"><font color="{theme.icolor}">{name}&nbsp;</font></td>\n'
-                        + f'        <td align="left"><font color="{theme.icolor}">{column.datatype}</font></td></tr>\n')
+                        + f'        <td align="left"><font color="{theme.icolor}">{datatype}</font></td></tr>\n')
                 else:
                     s += f'      <tr><td align="left"><font color="{theme.icolor}">{name}</font></td></tr>\n'
 
@@ -160,8 +182,8 @@ class Column:
         self.fkof = None            # points to the PK column on the other side
 
 
-    def getName(self):
-        return Table.getClassName(self.name)
+    def getName(self, useUpperCase, withQuotes=True):
+        return Table.getClassName(self.name, useUpperCase, withQuotes)
 
 
     def setDataType(self, datatype):
@@ -196,15 +218,20 @@ class Column:
 
 
     # outputs the column definition in a CREATE TABLE statement, for the parent table
-    def getCreateColumn(self):
+    def getCreateColumn(self, useUpperCase):
         nullable = "" if self.nullable or (self.ispk and len(self.table.pks) == 1) else " not null"
+        if useUpperCase: nullable = nullable.upper()
         identity = "" if not self.identity else " identity"
+        if useUpperCase: identity = identity.upper()
         pk = ""     # if not self.ispk or len(self.table.pks) >= 2 else " primary key"
+        if useUpperCase: pk = pk.upper()
+        datatype = self.datatype
+        if useUpperCase: datatype = datatype.upper()
         
         comment = self.comment.replace("'", "''")
-        if comment != '': comment = f" comment '{comment}'"
+        if comment != '': comment = f" COMMENT '{comment}'" if useUpperCase else f" comment '{comment}'"
 
-        return f"\n  {self.getName()} {self.datatype}{nullable}{identity}{pk}{comment}"
+        return f"\n  {self.getName(useUpperCase)} {datatype}{nullable}{identity}{pk}{comment}"
 
 
 # @st.cache_resource(show_spinner="Reading metadata...")
@@ -212,7 +239,7 @@ def importMetadata(database, schema):
     global session
     tables = {}
     if database == '' or schema == '': return tables
-    suffix = f"in schema {Table.getClassName(database)}.{Table.getClassName(schema)}"
+    suffix = f"in schema {Table.getClassName(database, False)}.{Table.getClassName(schema, False)}"
 
     # get tables
     query = f"show tables {suffix}"
@@ -293,25 +320,28 @@ def importMetadata(database, schema):
     return tables
 
 
-def createScript(tables, database, schema):
-    s = (f"use database {Table.getClassName(database)};\n"
-        + f"create or replace schema {Table.getClassName(database)}.{Table.getClassName(schema)};\n\n")
+def createScript(tables, database, schema, useUpperCase):
+    db = Table.getClassName(database, useUpperCase)
+    sch = f'{db}.{Table.getClassName(schema, useUpperCase)}'
+    if useUpperCase: s = f"USE DATABASE {db};\nCREATE OR REPLACE SCHEMA {sch};\n\n"
+    else: s = f"use database {db};\ncreate or replace schema {sch};\n\n"
+
     for name in tables:
-        s += tables[name].getCreateTable()
+        s += tables[name].getCreateTable(useUpperCase)
     for name in tables:
         for constraint in tables[name].fks:
-            s += tables[name].getFKs(constraint)
+            s += tables[name].getFKs(constraint, useUpperCase)
     return s
 
 
-def createGraph(tables, theme, showColumns, showTypes):
+def createGraph(tables, theme, showColumns, showTypes, useUpperCase):
     s = ('digraph {\n'
         + '  graph [ rankdir="LR" bgcolor="#ffffff" ]\n'
         + f'  node [ style="filled" shape="{theme.shape}" gradientangle="180" ]\n'
         + '  edge [ arrowhead="none" arrowtail="none" dir="both" ]\n\n')
 
     for name in tables:
-        s += tables[name].getDotShape(theme, showColumns, showTypes)
+        s += tables[name].getDotShape(theme, showColumns, showTypes, useUpperCase)
     s += "\n"
     for name in tables:
         s += tables[name].getDotLinks(theme)
@@ -356,21 +386,23 @@ def getDatabase():
     for row in results:
         names.append(str(row["name"]))
     sel = 0 if "Chinook" not in names else names.index("Chinook")
-    return st.sidebar.selectbox('Database', tuple(names), index=sel)
+    return st.sidebar.selectbox('Database', tuple(names), index=sel,
+        help="Select an existing database")
 
 
 def getSchema(database):
     global session
     names = []
     if database != "":
-        query = f"show schemas in database {Table.getClassName(database)}"
+        query = f"show schemas in database {Table.getClassName(database, False)}"
         results = session.sql(query).collect()
         for row in results:
             schemaName = str(row["name"])
             if schemaName != "INFORMATION_SCHEMA":
                 names.append(schemaName)
     sel = 0 if "PUBLIC" not in names else names.index("PUBLIC")
-    return st.sidebar.selectbox('Schema', tuple(names), index=sel)
+    return st.sidebar.selectbox('Schema', tuple(names), index=sel, 
+        help="Select a schema for the current database")
 
 st.set_page_config(layout="wide")
 session = getSession()
@@ -379,9 +411,14 @@ database = getDatabase()
 schema = getSchema(database)
 
 st.sidebar.divider()
-theme = st.sidebar.selectbox('Theme', tuple(themes.keys()), index=0)
-showColumns = st.sidebar.checkbox('Display Column Names', value=False)
-showTypes = st.sidebar.checkbox('Display Data Types', value=False)
+theme = st.sidebar.selectbox('Theme', tuple(themes.keys()), index=0, 
+    help="Select another color theme")
+showColumns = st.sidebar.checkbox('Display Column Names', value=False, 
+    help="Show columns in the expanded table shapes")
+showTypes = st.sidebar.checkbox('Display Data Types', value=False, 
+    help="Show column data type names when the table shapes are expanded")
+useUpperCase = st.sidebar.checkbox('Use Upper Case', value=False, 
+    help="Use upper case for table, column, and data type names, and for the script keywords")
 
 with st.spinner('Reading metadata...'):
     tables = importMetadata(database, schema)
@@ -391,8 +428,10 @@ elif len(tables) == 0:
     st.write("Found no tables in the current database and schema.")
 else:
     with st.spinner('Generating diagram and script...'):
+        graph = createGraph(tables, themes[theme], showColumns, showTypes, useUpperCase)
+        script = createScript(tables, database, schema, useUpperCase)
+
         tabERD, tabDOT, tabScript = st.tabs(["ERD Viewer", "DOT Code", "Create Script"])
-        s = createGraph(tables, themes[theme], showColumns, showTypes)
-        tabERD.graphviz_chart(s)
-        tabDOT.code(s, language="dot", line_numbers=True)
-        tabScript.code(createScript(tables, database, schema), language="sql", line_numbers=True)
+        tabERD.graphviz_chart(graph, use_container_width=True)
+        tabDOT.code(graph, language="dot", line_numbers=True)
+        tabScript.code(script, language="sql", line_numbers=True)
